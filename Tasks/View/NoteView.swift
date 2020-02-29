924b830a0ff2d79ab1fbfc5e18757071f62f2113
Dylan McCarthy <mccarthy.dylan@gmail.com>
2020-02-29 12:43:39 -0800

    Adding New Feature -- Notes

--- Tasks/View/NoteView.swift
+++ Tasks/View/NoteView.swift
@@ -1,9 +1,46 @@
-//
-//  NoteView.swift
-//  Tasks
-//
 //  Created by Dylan  on 2/29/20.
 //  Copyright © 2020 Dylan . All rights reserved.
-//
 
-import Foundation
+import UIKit
+
+final class NoteViewCell: UITableViewCell, CanWriteToDatabase {
+    
+    //MARK: - Properties
+    private var textView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = .systemBackground
+        textView.font = .preferredFont(for: .body, weight: .medium)
+        return textView
+    }()
+    
+    
+    //MARK: - Initializer
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    //MARK: - Layout
+    private func setupCell() {
+        contentView.addSubview(textView)
+        
+        let guide = contentView.layoutMarginsGuide
+        
+        NSLayoutConstraint.activate([
+            textView.heightAnchor.constraint(greaterThanOrEqualToConstant: 200),
+            textView.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: guide.trailingAnchor),
+        ])
+    }
+    
+    //MARK: - Interface
+    func configureNotesCell(noteText: String) {
+        self.textView.text = noteText
+    }
+}
